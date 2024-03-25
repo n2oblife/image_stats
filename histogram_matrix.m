f3041d58c752df8a3b3b7e39d607bc0159c24243
n2oblife <zackanit@gmail.com>
2024-03-25 09:45:25 +0100

    final stats analysis

--- histogram_matrix.m
+++ histogram_matrix.m
@@ -1,21 +1,21 @@
-function histo = histogram_mtx(img_mtx, depth=14)
+function histo = histogram_matrix(img_mtx, depth=14)
     #
-    # Usage : histogram_mtx(img_mtx, depth)
+    # Usage : histogram_matrix(img_mtx, depth)
     #   Computes the histogram of a matrix image according to depth of pixels
     # 
     # Parameters :
     #   img_mtx (array of arrays) : the image matrix to compute
-    #   depth (int) : depth of the pixels
+    #   depth (int) : depth of the pixels, default = 14
     #
     # Returns :
-    #   histo (array) : histogram of the matrix 
+    #   histo (array) : histogram of the matrix with 2^depth bins 
     [h,w] = size(img_mtx);
     histo = zeros(1,2^depth);
     printf("Computing global histogram ...\n");
     for i=1:h
         for j=1:w
             px_value = img_mtx(i,j); % dans le code de R sanchez, ajout de 2^14 / 2 ? Ne semble pas nécessaire
-            histo(px_value +1 ) += + 1;
+            histo(px_value +1 ) += + 1; % shift à cause de l'indexation de matlab
         endfor
     endfor
 endfunction
